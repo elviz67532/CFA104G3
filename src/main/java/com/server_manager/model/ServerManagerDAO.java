cafd45e8f99a9d6193cbb6214a2fb85522f79684
Sam Chang <sammy849754@gmail.com>
2022-02-13 22:20:30 +0800

    server manager
    1.login的.css
    2.web.xml 新增 ServerManagerServlet
    3.loginServer、serverManagerHom、ServerManagerServlet為
    可以成功登入畫面

--- src/main/java/com/server_manager/model/ServerManagerDAO.java
+++ src/main/java/com/server_manager/model/ServerManagerDAO.java
@@ -3,4 +3,6 @@ package com.server_manager.model;
 import core.dao.CoreDao;
 
 public interface ServerManagerDAO extends CoreDao<ServerManagerVO, Integer> {
+
+	public String findByAccount(String smgrAccount);
 }
