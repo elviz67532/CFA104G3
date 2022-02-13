994d3fe7be7d13f1df9782d492b06e540311397c
Elviz Chen <wl005771954@gmail.com>
2022-02-13 22:34:32 +0800

    修改搬家申請單、申請單圖片、修復部分BUG
    1.pom新增mail依賴
    2.修改前台搬家訂單申請頁面
    3.修改後台搬家訂單管理頁面
    4.新增HTML版本後台畫面
    5.修改前台申請單使用的語法
    6.修改搬家申請單照片MODEL

---
+++ src/main/java/com/move_request/controller/MoveRequestManageServlet.java
@@ -0,0 +1,93 @@
+package com.move_request.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+import com.move_photo.model.MovePhotoService;
+import com.move_photo.model.MovePhotoServiceImpl;
+import com.move_photo.model.MovePhotoVO;
+import com.move_request.model.MoveRequestManageTransVO;
+import com.move_request.model.MoveRequestService;
+import com.move_request.model.MoveRequestServiceImpl;
+import com.move_request.model.MoveRequestVO;
+import com.move_request.model.MoveTransVO;
+
+import core.CommonRes;
+import core.util.CommonUtil;
+
+//TODO
+public class MoveRequestManageServlet extends HttpServlet {
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doPost(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse response) throws ServletException, IOException {
+		//拆解json
+		String json = CommonUtil.jsonParse(req.getReader());
+		if (json == null || json .isEmpty()) {
+			return;
+		}
+		
+		Gson gson = new Gson();
+		MoveTransVO moveRequestVO = gson.fromJson(json , MoveTransVO.class);
+		if (moveRequestVO == null) {
+			return;
+		}
+		
+		// TODO 會員登入
+		
+		String mode = moveRequestVO.getMode();
+		if ("backManage".equals(mode)) {
+			Integer id = moveRequestVO.getId();
+			
+			//組回傳json
+			response.setCharacterEncoding("UTF8");
+			response.setContentType("text/html; charset=UTF-8");
+		
+			try {
+				MoveRequestService moveRequestService = new  MoveRequestServiceImpl();
+				MovePhotoService movePhotoService = new MovePhotoServiceImpl();
+				
+				// TODO 補取得MEMBER資料
+				
+				MoveRequestVO request = moveRequestService.getRequest(id);
+				List<MovePhotoVO> photos = movePhotoService.findAllPhotosByRequestId(id);
+				
+				MoveRequestManageTransVO moveRequestManageTransVO = new MoveRequestManageTransVO();
+				moveRequestManageTransVO.setMoveRequestVO(request);
+				moveRequestManageTransVO.setMoveRequestPhotoVOs(photos);
+				
+				CommonRes<MoveRequestManageTransVO> res = new CommonRes<>();
+				
+				res.setErrorCode("success");
+				res.setErrorMes("成功");
+				res.setBody(moveRequestManageTransVO);
+				String jsonStr = gson.toJson(res);
+				PrintWriter out = response.getWriter();
+		        out.print(jsonStr);          
+		        out.close();
+			} catch (Exception e) {
+				// TODO
+//				req.setAttribute("exception", e.getMessage());
+//				RequestDispatcher failureView = req.getRequestDispatcher("/front_end/move/moveRequest.jsp");
+//				failureView.forward(req, res);
+			}
+		}
+		
+		if ("frontManage".equals(mode)) {
+			
+		}
+	}
+}
---
+++ src/main/webapp/js/back_end/common/footer.js
@@ -0,0 +1,7 @@
+document.write('<footer class="sticky-footer bg-white">');
+document.write('	<div class="container my-auto">');
+document.write('		<div class="copyright text-center my-auto">');
+document.write('			<span>Copyright &copy; Entrust area 2022</span>');
+document.write('		</div>');
+document.write('	</div>');
+document.write('</footer>');
