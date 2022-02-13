dfb1667c9d10247a2ae4979e38b6483c2764ab84
Sam Chang <sammy849754@gmail.com>
2022-02-13 17:36:23 +0800

    商品和圖片上傳程式
    addPhoto
    sellerProduct
    ProductPhotoServiceImpl
    ProductPhotoServlet
    ProductServlet
    ProductDAO & ProductDAOImpl 增加自增主鍵

---
+++ src/main/java/com/product/controller/ProductServlet.java
@@ -0,0 +1,398 @@
+package com.product.controller;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.Timestamp;
+import java.util.Collection;
+import java.util.Enumeration;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.Part;
+
+import org.eclipse.jdt.internal.compiler.ast.WhileStatement;
+
+
+import com.product.model.ProductDAOImpl;
+import com.product.model.ProductServiceImpl;
+import com.product.model.ProductVO;
+
+
+public class ProductServlet extends HttpServlet {
+
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		doPost(req, res);
+	}
+
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		
+		
+		req.setCharacterEncoding("UTF-8");
+		String action = req.getParameter("action");
+			
+		
+		
+		if("getOne_For_Display".equals(action)) {
+
+			
+			List<String> errMsgs = new LinkedList<String>();
+			req.setAttribute("errMsgs", errMsgs);
+			
+			try {
+				/***************************1.接收請求參數 - 輸入格式的錯誤處理**********************/			
+				String str = req.getParameter("prodId");
+				if(str == null || (str.trim()).length() == 0) { // 沒有輸入資料 或 僅輸入空格
+					errMsgs.add("請輸入商品編號");
+				}
+				if (!errMsgs.isEmpty()) { // errorMsgs有內容
+					RequestDispatcher failureView = req
+							.getRequestDispatcher("/front_end/product/Seller.jsp");
+					failureView.forward(req, res);
+					return;
+				}
+				
+				// 格式不正確
+				Integer prodno = null;
+				try {
+					prodno = new Integer(str);
+				} catch (NumberFormatException e) {
+					errMsgs.add("商品編號格是不正確");
+				}
+				
+				if(!errMsgs.isEmpty()) {
+					RequestDispatcher failureView = req
+							.getRequestDispatcher("/front_end/product/Seller.jsp");
+					failureView.forward(req, res);
+					return;		
+				}
+				
+				/***************************2.開始查詢資料*****************************************/	
+				ProductServiceImpl productSvc = new ProductServiceImpl();
+				ProductVO prodVo = productSvc.getOneProduct(prodno);
+				if(prodVo == null) {
+					errMsgs.add("查無資料");
+				}
+				
+				if(!errMsgs.isEmpty()) {
+					RequestDispatcher failureView = req
+							.getRequestDispatcher("/front_end/product/Seller.jsp");
+					failureView.forward(req, res);
+					return;//程式中斷				
+				}
+				
+				/***************************3.查詢完成,準備轉交(Send the Success view)*************/
+				req.setAttribute("productVO", prodVo);
+				String url = "/front_end/product/listOneProduct.jsp";
+				RequestDispatcher successView = req.getRequestDispatcher(url); // 成功轉交 listOneEmp.jsp
+				successView.forward(req, res);
+			} catch (Exception e) {
+				errMsgs.add("無法取得資料" + e.getMessage());
+				RequestDispatcher failureView = req
+						.getRequestDispatcher("/front_end/product/Seller.jsp");
+				failureView.forward(req, res);
+				return;				
+			}	
+			
+		}
+		
+		if("get_Display_From_Type".equals(action)) {
+			
+			List<String> errMsgs = new LinkedList<String>();
+			req.setAttribute("errMsgs", errMsgs);
+			
+			try {
+				/***************************1.接收請求參數**********************/
+				Integer prodType = Integer.valueOf(req.getParameter("prodType"));
+				
+				/***************************2.開始查詢資料*****************************************/	
+				ProductServiceImpl productSvc = new ProductServiceImpl();
+				List<ProductVO> list = productSvc.getProductsByType(prodType);
+				
+				if(list == null) {
+					errMsgs.add("目前無這類商品");
+				}
+				if(!errMsgs.isEmpty()) {
+					RequestDispatcher failureView = req.getRequestDispatcher("/front_end/product/Seller.jsp");
+					failureView.forward(req, res);
+					return;
+				}
+				/***************************3.查詢完成,準備轉交(Send the Success view)*************/
+				req.setAttribute("list", list);
+				String url = "/front_end/product/listOneTypeProduct.jsp";
+				RequestDispatcher successView = req.getRequestDispatcher(url);
+				successView.forward(req, res);
+				
+			} catch (Exception e) {
+				errMsgs.add("無法取得資料" + e.getMessage());
+				RequestDispatcher failureView = req.getRequestDispatcher("/front_end/product/Seller.jsp");
+				failureView.forward(req, res);
+				return;
+			}
+			
+		}
+
+		if("getOne_For_Update".equals(action)) {
+			
+			List<String> errMsgs = new LinkedList<String>();
+			req.setAttribute("errMsgs", errMsgs); // 放置到request裡面	
+			
+			try {
+				/***************************1.接收請求參數****************************************/
+				Integer prodId = Integer.valueOf(req.getParameter("prodId"));
+//				System.out.println("ProductServlet-getoneforupdate: " + prodId); // 有取得數值
+				/***************************2.開始查詢資料****************************************/
+				ProductServiceImpl productSvc = new ProductServiceImpl();
+				ProductVO productVO = productSvc.getOneProduct(prodId);
+				
+				/***************************3.查詢完成，準備轉交****************************************/
+				req.setAttribute("productVO", productVO);
+				String url = "/back_end/product/update_product_input.jsp";
+				RequestDispatcher successView = req.getRequestDispatcher(url);
+				successView.forward(req, res);
+				
+				/***************************其他可能的錯誤處理**********************************/
+			} catch (Exception e) {
+				errMsgs.add("無法取得要修改的資料:" + e.getMessage());
+				RequestDispatcher failView = req
+						.getRequestDispatcher("/front_end/product/listAll.jsp");
+				failView.forward(req, res);
+			} 
+			
+		}	
+		
+		if("update".equals(action)) {
+			
+			List<String> errMsgs = new LinkedList<String>();
+			req.setAttribute("errMsgs", errMsgs); // 放置到request裡面	
+			
+			/***************************1.接收請求參數****************************************/
+			Integer prodId = Integer.valueOf(req.getParameter("prodId").trim());
+			
+			Integer memId = null;
+			try {
+				memId = Integer.valueOf(req.getParameter("memId").trim());
+			} catch (NumberFormatException e) {
+				/*檢查是否為 number*/
+				errMsgs.add("會員編號請填入正整數");
+			}
+
+			Integer prodType = Integer.valueOf(req.getParameter("prodType").trim());
+			/*檢查是否為 0 1 2 3 4*/
+
+			String prodDesc = req.getParameter("prodDesc").trim();
+			if(prodDesc == null || prodDesc.trim().length() == 0) {
+				errMsgs.add("商品敘述請勿空白");
+			}
+			
+			Integer prodPrice = null;
+			try {
+				prodPrice = Integer.valueOf(req.getParameter("prodPrice"));
+			} catch (NumberFormatException e) {
+				errMsgs.add("請填入商品價格");
+			}
+			
+			String prodName = req.getParameter("prodName").trim();
+			if (prodName == null || prodDesc.trim().length() == 0) {
+				errMsgs.add("請填入商品名稱");
+			}
+
+			Timestamp prodUptime = null;
+			try {
+				prodUptime = Timestamp.valueOf(req.getParameter("prodUpdate").trim());
+			} catch (IllegalArgumentException e) {
+				prodUptime = new Timestamp(System.currentTimeMillis());
+			}
+
+
+			String prodLoc = req.getParameter("prodLoc").trim();
+			if (prodLoc == null || prodLoc.trim().length() == 0) {
+				errMsgs.add("商品所在地請勿空白");
+			}
+
+			Integer prodStatus = Integer.valueOf(req.getParameter("prodStatus").trim());
+
+
+			/*打包VO預防錯誤輸入時重新打資料*/
+			ProductVO productVO = new ProductVO();
+			productVO.setDescription(prodDesc);
+			productVO.setLocation(prodLoc);
+			productVO.setSellerMemberId(memId);
+			productVO.setName(prodName);
+			productVO.setPrice(prodPrice);
+			productVO.setStatus(prodStatus);
+			productVO.setType(prodType);
+			productVO.setLaunchedDate(prodUptime);
+			
+			if(!errMsgs.isEmpty()) {
+				req.setAttribute("prodVO", productVO);
+				RequestDispatcher failureView = req
+						.getRequestDispatcher("/front_end/product/update_product_input.jsp");
+				failureView.forward(req, res);
+				return;
+			}
+			
+			/***************************2.開始修改資料*****************************************/			
+			ProductServiceImpl productSvc = new ProductServiceImpl();
+			productVO = productSvc.updateProduct(prodId, prodName, memId, prodType, prodDesc,
+					prodPrice, prodUptime, prodLoc, prodStatus);
+			
+			/***************************3.修改完成,準備轉交(Send the Success view)*************/		
+			req.setAttribute("productVO", productVO);
+			String successUrl = "/front_end/product/listOneProduct.jsp";
+			RequestDispatcher successDispatcher = req.getRequestDispatcher(successUrl);
+			successDispatcher.forward(req, res);
+		}
+
+		
+		if ("insert".equals(action)) {
+			/* 新增商品
+			 * 控制器 需要的錯誤訊息站存區 請求參數 會員id 商品類別 商品敘述 價格 商品名稱 上架時間 所在地 商品目前狀態
+			 * */
+			
+			List<String> errMsgs = new LinkedList<String>();
+			req.setAttribute("errMsgs", errMsgs); // 放置到request裡面
+			
+			Integer memId = null;
+			try {
+				memId = Integer.valueOf(req.getParameter("memId").trim());
+			} catch (NumberFormatException e) {
+				/*檢查是否為 number*/
+				errMsgs.add("會員編號請填入正整數");
+			}
+
+
+			Integer prodType = Integer.valueOf(req.getParameter("prodType").trim());
+			/*檢查是否為 0 1 2 3 4*/
+
+			String prodDesc = req.getParameter("prodDesc").trim();
+			if(prodDesc == null || prodDesc.trim().length() == 0) {
+				errMsgs.add("商品敘述請勿空白");
+			}
+			
+			Integer prodPrice = null;
+			try {
+				prodPrice = Integer.valueOf(req.getParameter("prodPrice"));
+			} catch (NumberFormatException e) {
+				errMsgs.add("請填入商品價格");
+			}
+			
+			String prodName = req.getParameter("prodName").trim();
+			if (prodName == null || prodDesc.trim().length() == 0) {
+				errMsgs.add("請填入商品名稱");
+			}
+
+			Timestamp prodUptime = null;
+			try {
+			//	prodUptime = Timestamp.valueOf(req.getParameter("prodUpdate").trim());
+				prodUptime = new Timestamp(System.currentTimeMillis());
+			} catch (IllegalArgumentException e) {
+				prodUptime = new Timestamp(System.currentTimeMillis());
+			}
+
+
+			String prodLoc = req.getParameter("prodLoc").trim();
+			if (prodLoc == null || prodLoc.trim().length() == 0) {
+				errMsgs.add("商品所在地請勿空白");
+			}
+
+			//Integer prodStatus = Integer.valueOf(req.getParameter("prodStatus").trim());
+			Integer prodStatus = 0;
+			
+			/*打包VO預防錯誤輸入時重新打資料*/
+			ProductVO productVO = new ProductVO();
+			productVO.setDescription(prodDesc);
+			productVO.setLocation(prodLoc);
+			productVO.setSellerMemberId(memId);
+			productVO.setName(prodName);
+			productVO.setPrice(prodPrice);
+			productVO.setStatus(prodStatus);
+			productVO.setType(prodType);
+			productVO.setLaunchedDate(prodUptime);
+			
+			if(!errMsgs.isEmpty()) {
+				req.setAttribute("productVO", productVO);
+				RequestDispatcher failureView = req
+						.getRequestDispatcher("/front_end/product/SellerProduct.jsp");
+				failureView.forward(req, res);
+				return;
+			}
+			ProductServiceImpl productSvc = new ProductServiceImpl();
+			String key = productSvc.addProduct(prodName, memId, prodType, prodDesc, prodPrice, prodUptime, prodLoc, prodStatus);
+			System.out.println("key="+key); // 自增主鍵
+			HttpSession session = req.getSession();
+			session.setAttribute("key", key);
+			String successUrl = "/front_end/product/addPhoto.jsp";
+			RequestDispatcher successDispatcher = req.getRequestDispatcher(successUrl);
+			successDispatcher.forward(req, res);
+		}
+		
+
+		if("delete".equals(action)) {
+			
+			List<String> errMsgs = new LinkedList<String>();
+			req.setAttribute("errMsgs", errMsgs);
+			
+			try {
+				/***************************1.接收請求參數*****************************************/
+				Integer prodId = Integer.valueOf(req.getParameter("prodId"));
+
+				/***************************2.開始刪除資料*****************************************/
+				ProductServiceImpl productSvc = new ProductServiceImpl();
+				productSvc.delete(prodId);
+				
+				/***************************3.刪除完成,準備轉交(Send the Success view)*************/	
+				String url = "/back_end/product/listAll.jsp";
+				RequestDispatcher successview = req.getRequestDispatcher(url);
+				successview.forward(req, res);
+			} catch (Exception e) {
+				errMsgs.add("刪除資料失敗" + e.getMessage());
+				String url = "/front_end/product/listAll.jsp";				
+				RequestDispatcher failureView = req.getRequestDispatcher(url);
+				failureView.forward(req, res);
+			} 
+			
+		}
+
+//		單一欄位的複合查詢???
+//		if("listProd_ByCompositeQuery".equals(action)) {
+//			List<String> errMsgs = new LinkedList<String>();
+//			// Store this set in the request scope, in case we need to
+//			// send the ErrorPage view.
+//			req.setAttribute("errMsgs", errMsgs);
+//			
+//			try {
+//				
+//				/***************************1.將輸入資料轉為Map**********************************/ 
+//				//採用Map<String,String[]> getParameterMap()的方法 
+//				//注意:an immutable java.util.Map 
+//				Map<String, String[]> map = req.getParameterMap();
+//				
+//				/***************************2.開始複合查詢***************************************/
+//				ProductServiceImpl prodSvc = new ProductServiceImpl();
+//				List<EmpVO> list  = prodSvc.getAll(map);
+//				
+//				/***************************3.查詢完成,準備轉交(Send the Success view)************/
+//				req.setAttribute("listEmps_ByCompositeQuery", list); // 資料庫取出的list物件,存入request
+//				RequestDispatcher successView = req.getRequestDispatcher("/emp/listEmps_ByCompositeQuery.jsp"); // 成功轉交listEmps_ByCompositeQuery.jsp
+//				successView.forward(req, res);
+//				
+//				/***************************其他可能的錯誤處理**********************************/
+//			} catch (Exception e) {
+//				errMsgs.add(e.getMessage());
+//				RequestDispatcher failureView = req
+//						.getRequestDispatcher("/select_page.jsp");
+//				failureView.forward(req, res);
+//			}			
+//		}
+	}
+
+
+}
---
+++ src/main/webapp/front_end/product/sellerProduct.jsp
@@ -0,0 +1,183 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ page import="java.util.*"%>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>委域</title>
+    <link rel="icon" type="image/x-icon" href="asset/favicon.ico" />
+    <!-- Font Awesome icons (free version)-->
+    <script src="https://use.fontawesome.com/releases/v5.15.4/js/all.js" crossorigin="anonymous"></script>
+    <!-- Google fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic" rel="stylesheet"
+        type="text/css" />
+    <link
+        href="https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800"
+        rel="stylesheet" type="text/css" />
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="<%=request.getContextPath()%>/vendor/bootstrap/css/styles.css" rel="stylesheet" />
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/checkout/">
+	<style>
+		.bd-placeholder-img {
+			font-size: 1.125rem;
+			text-anchor: middle;
+			-webkit-user-select: none;
+			-moz-user-select: none;
+			user-select: none;
+		}
+		
+		@media ( min-width : 768px) {
+			.bd-placeholder-img-lg {
+				font-size: 3.5rem;
+			}
+		}
+	</style>
+	<style>
+		.SEARCHFOR {
+			padding: 10px;
+			margin-bottom: 30px;
+		}
+		
+		#pagination {
+			bottom: 0;
+		}
+		
+		.d-block {
+			height: 30%;
+		}
+		
+		.myImg {
+			height: 400px;
+			background-repeat: no-repeat;
+			background-attachment: fixed;
+			background-position: center;
+			background-size: cover;
+		}
+	</style>
+</head>
+
+<body>
+    <!-- Navigation-->
+    <!-- nav -->
+	<jsp:include page="/front_end/common/navigation.jsp"></jsp:include>
+
+    <!-- Page Header-->
+    <header class="masthead" style="background-image: url('<%=request.getContextPath()%>/asset/img/bgHome01.jpg')">
+        <div class="container position-relative px-4 px-lg-5">
+            <div class="row gx-4 gx-lg-5 justify-content-center">
+                <div class="col-md-10 col-lg-8 col-xl-7">
+                    <div class="site-heading">
+                        <h1>New Life</h1>
+                        <span class="subheading">迎 接 全 新 的 人 生</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+   
+   	<!-- 主體畫面設計  -->
+	<%-- 錯誤表列 --%>
+	<c:if test="${not empty errMsgs}">
+		<font style="color:red">請修正以下錯誤</font>
+		<ul>
+			<c:forEach var="msg" items="${errMsgs}">
+				<li style="color:red">${msg}</li>
+			</c:forEach>
+		</ul>
+	</c:if>
+
+	<!-- nav -->
+	<jsp:include page="/front_end/common/navigation.jsp"></jsp:include>
+	<div class="container">
+		<main>
+			<div class="py-5 text-center">
+				<h2>商品新增</h2>
+			</div>
+				<!-- 目前手動輸入 -->
+				<!-- 取得會員ID -->
+				<jsp:useBean id="memVO" class="com.member.model.MemberVO"/>
+				<div class="col-12">
+					<FORM METHOD="post" ACTION="<%=request.getContextPath()%>/product/ProductServlet" class="needs-validation" 
+						  name="form1" novalidate>
+						<div class="row g-3">
+							<div class="col-sm-6">
+								<label for="memId" class="form-label">會員編號:</label><br> 
+								<input type="text" value="" class="form-control" name="memId"/>
+							</div>
+							<!-- 商品名稱 -->
+							<div class="col-sm-6">
+								<label for="lastName" class="form-label">商品名稱</label> 
+								<input type="text" name="prodName" class="form-control" required/>
+								<div class="invalid-feedback">Valid last name is required.</div>
+							</div>
+							<!-- 商品內容 -->
+							<div class="col-12">
+								<label for="lastName" class="form-label">商品內容</label> 
+								<input type="text" name="prodDesc" class="form-control" required>
+								<div class="invalid-feedback">Valid last name is required.</div>
+							</div>
+							<!-- 商品地址 -->
+							<div class="col-sm-6">
+								<label for="address" class="form-label">地址</label> 
+								<input type="text" name="prodLoc" class="form-control" id="address" required>
+							</div>
+							<!-- 商品價格 -->
+							<div class="col-sm-6">
+								<label for="address" class="form-label">金額</label> <input
+									type="text" name="prodPrice" 
+									class="form-control" id="prodPrice"
+									placeholder="NT$" required>
+							</div>
+							<!-- 商品種類 -->
+							<div class="col-md-5">
+								<label for="country" class="form-label">類型</label> 
+								<select name="prodType" class="form-select" id="country" required>
+									<option>Choose...</option>
+									<option value=4>電器類</option>
+									<option value=2>寢具類</option>
+									<option value=0>其他</option>
+								</select>
+							</div>
+						</div>
+						<!-- 送出文字資訊 -->
+						<br><br>
+						<input type="hidden" name="action" value="insert">	
+						<input class="w-100 btn btn-primary btn-lg" type="submit" value="送出">
+					</FORM>			
+				</div>
+			</div>
+		</main>
+
+	<footer class="my-5 pt-5 text-muted text-center text-small">
+		<p class="mb-1">&copy; 2017–2021 Company Name</p>
+		<ul class="list-inline">
+			<li class="list-inline-item"><a href="#">Privacy</a></li>
+			<li class="list-inline-item"><a href="#">Terms</a></li>
+			<li class="list-inline-item"><a href="#">Support</a></li>
+		</ul>
+	</footer>
+</div>
+
+
+	<script
+		src="<%=request.getContextPath()%>/vendors/bootstrap/css/bootstrap.min.css"></script>
+
+	<script src="form-validation.js"></script>
+ 
+
+   
+    <!-- Footer-->
+   	<jsp:include page="/front_end/common/footer.jsp"></jsp:include>
+    <!-- Bootstrap core JS-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- Core theme JS-->
+    <script src="<%=request.getContextPath()%>/js/front_end/scripts.js"></script>
+</body>
+
+</html>
