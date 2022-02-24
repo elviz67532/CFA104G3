76033cd5a76cabbccd7f804c260b1532f861ee9e
William <williamtu871110@gmail.com>
2022-02-24 16:58:54 +0800

    怡理用我的電腦幫保哥改它的城市
    饒口吧

--- src/main/java/com/activity_photo/model/ActivityPhotoService.java
+++ src/main/java/com/activity_photo/model/ActivityPhotoService.java
@@ -6,6 +6,8 @@ public interface ActivityPhotoService {
 	
 	public ActivityPhotoVO updateActPhoto(Integer activityId, byte[] photo); 
 
-	public ActivityPhotoVO findActPhoto(Integer photoId);  
+	public ActivityPhotoVO findActPhoto(Integer photoId);
+
+	public ActivityPhotoVO findActPhotoByActId(int actId);  
 	
 }
